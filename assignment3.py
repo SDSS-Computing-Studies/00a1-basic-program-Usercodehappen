45660c3e1beaf0a257e8f7d4866e0e07e5ff3d0c
Lj <55156461+Usercodehappen@users.noreply.github.com>
2021-02-08 10:15:20 -0800

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -3,6 +3,6 @@
 #  Use of the the print() and print( , end="") commands to display output.
 #  Check the README.md file for the sample output
 
-print("Hello world!" , end=" ")
+print(end=" ", "Hello world!"  )
 print("This is how multiple print statements are joined")
 
